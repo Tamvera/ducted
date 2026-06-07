a82cbd9e6130a27233e55f157bdc0dee45c4a380
Colin Alston <1044161+calston@users.noreply.github.com>
2026-06-07 19:49:32 +0100

    Revert "Try some new docs and stuff"
    This reverts commit 538e282b578ce58b49d5496d37def61fd9853bc2.

---
+++ duct/sources/mail.py
@@ -0,0 +1,11 @@
+"""
+.. module:: mail
+   :platform: Unix
+   :synopsis: A source module for mailserver tests
+
+.. moduleauthor:: Colin Alston <colin@tamvera.com>
+"""
+raise NotImplementedError(
+    "Mail checks (SMTP/IMAP round-trip) are not yet implemented for Python 3. "
+    "Use aiosmtplib and an asyncio IMAP library to implement this module."
+)
